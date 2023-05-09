49b85f7c8b6577cb1eda06c6f304761272dc5770
kick250 <kicklobat@gmail.com>
2023-05-08 21:49:32 -0300

    testar dependencia do exercicio 11

--- test_ex_11.py
+++ test_ex_11.py
@@ -1,10 +1,11 @@
-import ex_11
+# import ex_11
+import helpers
 from exceptions import InvalidVectorException
 
 def test_convert_vector_from_text():
   vector_text = "1, 67, 78, 34, 90, 876, 0, 12, 8, 3, 56, 987, 3, 2,45,12,10, 45"
 
-  vector = ex_11.convert_vector_from_text(vector_text)
+  vector = helpers.convert_vector_from_text(vector_text)
   expected_vector = (1, 67, 78, 34, 90, 876, 0, 12, 8, 3, 56, 987, 3, 2, 45, 12, 10, 45)
   assert vector == expected_vector
 
@@ -12,7 +13,7 @@ def test_convert_vector_from_text_when_invalid():
   vector_text = "1, Batata, 78"
 
   try:
-    ex_11.convert_vector_from_text(vector_text)
+    helpers.convert_vector_from_text(vector_text)
     assert False
   except InvalidVectorException as exception:
     assert True
